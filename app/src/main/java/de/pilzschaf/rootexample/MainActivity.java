8de30fafb5b7de0b535c9b47ec184410bb0cf5e9
pilzschaf <pilzschaf@t-online.de>
2016-06-20 14:38:58 +0200

    added RootTools as library added examples to use RootTools to run root commands and normal shell commands

--- app/src/main/java/de/pilzschaf/rootexample/MainActivity.java
+++ app/src/main/java/de/pilzschaf/rootexample/MainActivity.java
@@ -25,6 +25,13 @@ package de.pilzschaf.rootexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.stericson.RootShell.exceptions.RootDeniedException;
+import com.stericson.RootShell.execution.Command;
+import com.stericson.RootTools.RootTools;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 import de.pilzschaf.rootexample.shell.CommandExecutionException;
 import de.pilzschaf.rootexample.shell.CommandOutput;
 import de.pilzschaf.rootexample.shell.Shell;
@@ -35,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Simple Example using the shell package
         Shell shell = new Shell();
         try {
             //Command requires root privileges to output
@@ -47,6 +56,56 @@ public class MainActivity extends AppCompatActivity {
         } catch (CommandExecutionException e) {
             System.out.println("Error executing command");
             System.out.println("Device not rooted?");
+            e.printStackTrace();
+        }
+
+        //Advanced example using the RootToolsLibrary
+        //Available at https://github.com/Stericson/RootTools
+        System.out.println("Root available: "+RootTools.isRootAvailable());
+        System.out.println("Busybox available: "+RootTools.isBusyboxAvailable());
+
+        //Does a request for root access
+        System.out.println("Root access given: "+RootTools.isAccessGiven());
+
+        //Run non root command
+        Command command1 = new Command(0, "ls -la");
+        try {
+            RootTools.getShell(false).add(command1);
+        } catch (IOException | RootDeniedException | TimeoutException e) {
+            e.printStackTrace();
+        }
+
+        //Run a root command
+        Command command2 = new Command(0, "ls -la /data");
+        try {
+            RootTools.getShell(true).add(command2);
+        } catch (IOException | RootDeniedException | TimeoutException e){
+            e.printStackTrace();
+        }
+
+        //Run command and print output
+        Command command3 = new Command(0, "ls -la /data")
+        {
+            @Override
+            public void commandOutput(int id, String line){
+                System.out.println(line);
+                super.commandOutput(id, line);
+            }
+            @Override
+            public void commandTerminated(int id, String reason) {
+                System.out.println("Command terminated because: "+reason);
+                super.commandTerminated(id, reason);
+            }
+
+            @Override
+            public void commandCompleted(int id, int exitCode) {
+                System.out.println("Command completed with exit code: "+exitCode);
+                super.commandCompleted(id, exitCode);
+            }
+        };
+        try {
+            RootTools.getShell(true).add(command3);
+        } catch (IOException | RootDeniedException | TimeoutException e) {
             e.printStackTrace();
         }
     }
